53d397d5c9dd61399e6c76f44b48542bb8095496
junne.wanja@student.moringaschool.com <junne.wanja@student.moringaschool.com>
2024-09-03 11:38:17 +0300

    updated task

--- add_numbers.py
+++ add_numbers.py
@@ -5,6 +5,5 @@ def add_numbers(num1, num2):
     return num1 + num2
 
 # Example usage:
-if __name__ == "__main__":
-    print("Testing add_numbers function")
-    print(add_numbers(20, 17))  # Output should be 37
+
+ print(add_numbers(20, 17))  # Output should be 37
